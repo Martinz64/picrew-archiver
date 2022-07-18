49629d86fe784e4029848dc22d4390c37b018a38
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2022-07-18 10:14:26 +0800

    fix: download and encoding errors

--- generate_html.py
+++ generate_html.py
@@ -1,4 +1,5 @@
 import os
+from sys import argv
 
 dir = argv[1]
 
@@ -17,11 +18,11 @@ for cat in categories:
     items = []
 
     category_thumbnail = ""
-    
+
     for file in fItems:
         if "p_" in file:
             category_thumbnail = cat + '/' + file
-            
+
         if os.path.isdir(dir+'/'+cat+'/'+file):
             cdir = dir+'/'+cat+'/'+file
             cdir2 = cat+'/'+file
@@ -34,15 +35,16 @@ for cat in categories:
                         item_thumbnail = cdir2 + '/' + img
                     else:
                         variants.append(cdir2 + '/' + img)
-                
-            items.append((item_thumbnail,variants,cdir,cdir2)) #cdir is the base path
-            
-            #print(item_thumbnail)        
+
+            # cdir is the base path
+            items.append((item_thumbnail, variants, cdir, cdir2))
+
+            # print(item_thumbnail)
     #f.write("<img src=\"" + category_thumbnail + "\"/>" )
-    catgs.append((category_thumbnail,items, cat))
+    catgs.append((category_thumbnail, items, cat))
 
 
-with open(dir+'/output.html', 'w') as f:
+with open(dir+'/output.html', 'w', encoding="utf-8") as f:
     f.write('''
     <!DOCTYPE html>
 <html lang="en">
@@ -100,22 +102,23 @@ with open(dir+'/output.html', 'w') as f:
         f.write('<li>')
         f.write('<div class="hdr">')
         f.write('<img class="header" src="' + cat[0] + '" />')
-        f.write('<h3 class="title">' + ''.join(cat[2].split('-')[1:]) + '</h3>')
+        f.write('<h3 class="title">' +
+                ''.join(cat[2].split('-')[1:]) + '</h3>')
         f.write('</div>')
         f.write('<div class="cat_wrapper">')
 
         for item in cat[1]:
-            #f.write('<tr>')
+            # f.write('<tr>')
             #f.write('<td class="thumbnail"><a href="' + item[2] + '/index.html"><img src="' + item[0] + '" /></a></td>')
-            #for variant in item[1]:
+            # for variant in item[1]:
             #    f.write('<td><img class="content" src="' + variant + '" /></td>')
 
-            #f.write('</tr>')
-            f.write('<div class="thumbnail"><a href="' + item[3] + '/index.html" target="_blank"><img src="' + item[0] + '" /></a></div>')
+            # f.write('</tr>')
+            f.write('<div class="thumbnail"><a href="' +
+                    item[3] + '/index.html" target="_blank"><img src="' + item[0] + '" /></a></div>')
 
         f.write('</div>')
         f.write('</li>')
-    
 
     f.write('''
     </ul>
@@ -157,12 +160,14 @@ html_footer = '''
 
 for cat in catgs:
     for item in cat[1]:
-        with open(item[2] + '/index.html','w') as f:
+        with open(item[2] + '/index.html', 'w', encoding="utf-8") as f:
             f.write(html_header)
-            f.write('<img class="thumbnail" src="' + item[0].split('/')[-1] + '" />')
+            f.write('<img class="thumbnail" src="' +
+                    item[0].split('/')[-1] + '" />')
             f.write('<div class="main">')
 
             for variant in item[1]:
-                f.write('<img class="content" src="' + variant.split('/')[-1] + '" />')
+                f.write('<img class="content" src="' +
+                        variant.split('/')[-1] + '" />')
             f.write('</div>')
             f.write(html_footer)
--- get_info.py
+++ get_info.py
@@ -3,15 +3,18 @@ import re
 import json
 import os
 
+
 def mkdir(dir):
     if not os.path.exists(dir):
-        os.makedirs(dir) 
+        os.makedirs(dir)
+
 
 def download(url: str, dest_folder: str):
     if not os.path.exists(dest_folder):
         os.makedirs(dest_folder)  # create folder if it does not exist
 
-    filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
+    # be careful with file names
+    filename = url.split('/')[-1].replace(" ", "_")
     file_path = os.path.join(dest_folder, filename)
 
     if os.path.exists(file_path):
@@ -30,6 +33,9 @@ def download(url: str, dest_folder: str):
     else:  # HTTP status code 4XX/5XX
         print("Download failed: status code {}\n{}".format(r.status_code, r.text))
 
+
+output = open("urls.txt", "w", encoding="utf-8") # To avoid encoding error, this happens when OS is not using utf-8
+
 id = os.sys.argv[1]
 
 main_page = requests.get("https://picrew.me/image_maker/"+id).text
@@ -38,40 +44,57 @@ key_pattern = re.compile("release_key:\"([A-Za-z0-9]+)\"")
 key = key_pattern.findall(main_page)[0]
 
 print("https://picrew.me/app/image_maker/" + id + "/" + key + "/cf.json")
-cf_data = requests.get("https://picrew.me/app/image_maker/" + id + "/" + key + "/cf.json").text
+output.write("https://picrew.me/app/image_maker/" +
+             id + "/" + key + "/cf.json\n")
+cf_data = requests.get(
+    "https://picrew.me/app/image_maker/" + id + "/" + key + "/cf.json").text
 cf = json.loads(cf_data)
 
-img_data = requests.get("https://picrew.me/app/image_maker/" + id + "/" + key + "/img.json").text
+img_data = requests.get(
+    "https://picrew.me/app/image_maker/" + id + "/" + key + "/img.json").text
 img = json.loads(img_data)
 
-#print(cf)
+# print(cf)
 
-base_url = cf['baseUrl']
+base_url = "https://picrew.me"
 part_list = cf['pList']
 
 try:
     for p in part_list:
-        print(p['pId'],p['pNm'],base_url + p['thumbUrl'])
-        path =id+"/"+str(p['pId'])+'-'+p['pNm']
-        #mkdir(path)
-        #download(base_url + p['thumbUrl'],dest_folder=path)
-        print("DL: ["+base_url + p['thumbUrl']+"] -> [" + path + "]")
+        path = id+"/"+str(p['pId'])+'-'+p['pNm']
+        if p['thumbUrl']:  # Pass if thumbnail is null
+            print(p['pId'], p['pNm'], base_url + p['thumbUrl'])
+            output.write(str(p['pId'])+' '+str(p['pNm'])+' ' +
+                         base_url + str(p['thumbUrl']) + '\n')
+            # mkdir(path)
+            #download(base_url + p['thumbUrl'],dest_folder=path)
+            print("DL: ["+base_url + p['thumbUrl']+"] -> [" + path + "]")
+            output.write(
+                "DL: ["+base_url + str(p['thumbUrl'])+"] -> [" + path + "]\n")
 
         for item in p['items']:
-            item_path = path + "/" + str(item['itmId'])
-            #download(base_url + item['thumbUrl'],dest_folder=item_path)
-            print("DL: ["+base_url + item['thumbUrl']+"] -> [" + item_path + "]")
-            print("-",item['itmId'], base_url + item['thumbUrl'])
+            if item['thumbUrl']:  # Pass if thumbnail is null
+                item_path = path + "/" + str(item['itmId'])
+                #download(base_url + item['thumbUrl'],dest_folder=item_path)
+                print("DL: ["+base_url + item['thumbUrl'] +
+                      "] -> [" + item_path + "]")
+                output.write(
+                    "DL: ["+base_url + str(item['thumbUrl'])+"] -> [" + item_path + "]\n")
+                print("-", item['itmId'], base_url + item['thumbUrl'])
+                output.write("-"+' '+str(item['itmId'])+' ' +
+                             base_url + str(item['thumbUrl']) + '\n')
 
             try:
                 item_dict = img['lst'][str(item['itmId'])]
                 for k1 in item_dict.keys():
                     for k2 in item_dict[k1].keys():
                         url = base_url + item_dict[k1][k2]['url']
-                        #download(url,dest_folder=item_path)
+                        # download(url,dest_folder=item_path)
                         print("DL: ["+url+"] -> [" + item_path + "]")
+                        output.write("DL: ["+url+"] -> [" + item_path + "]\n")
             except:
-                print("parse error:",str(item['itmId']))
-                
+                print("parse error:", str(item['itmId']))
+
 except:
-    ""
+    import traceback
+    traceback.print_exc()
